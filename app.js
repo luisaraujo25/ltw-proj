8d693b3c8fbdc7017d6cc479b46121b830a2c9bf
Luísa Araújo <up201904996@fe.up.pt>
2021-11-16 11:52:56 +0000

    tabuleiro+regras

---
+++ app.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open");
+const regras_container = document.getElementById("regras_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+    regras_container.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    regras_container.style.display = "none";
+});i
