0400a55864c3e1acffc8da97dfb1490a8329e64a
Luísa Araújo <up201904996@fe.up.pt>
2022-01-11 11:46:55 +0000

    removing rules + FIRST REQUEST WORKINGgit add -A FETCH(RANKINGS)

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const open = document.getElementById("open");
 const regras_container = document.getElementById("regras_container");
 const close = document.getElementById("close");
 
-open.addEventListener("click", () => {
-    regras_container.style.display = "flex";
-});
-
-close.addEventListener("click", () => {
-    regras_container.style.display = "none";
-});
-
 
 //BOT - MINIMAX - MINIMIZE THE LOSS
 function mybot(){
--- request_exp.js
+++ request_exp.js
@@ -1,22 +1,30 @@
-// async function postData(url = '', data = {}) {
-//     // Default options are marked with *
-//     const response = await fetch(url, {
-//       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//       mode: 'cors', // no-cors, *cors, same-origin
-//       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//       credentials: 'same-origin', // include, *same-origin, omit
-//       headers: {
-//         'Content-Type': 'application/json'
-//         // 'Content-Type': 'application/x-www-form-urlencoded',
-//       },
-//       redirect: 'follow', // manual, *follow, error
-//       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//       body: JSON.stringify(data) // body data type must match "Content-Type" header
-//     });
-//     return response.json(); // parses JSON response into native JavaScript objects
-//   }
-  
-//   postData('https://example.com/answer', { answer: 42 })
-//     .then(data => {
-//       console.log(data); // JSON data parsed by `data.json()` call
-//     });
+const url = "http://twserver.alunos.dcc.fc.up.pt:8008/";
+
+// fetch(url);
+
+const ranking = url + "ranking";
+
+async function getRankings() {
+    // let url = 'ranking.json';
+    try {
+        // let res = await fetch(url);
+        let res = await fetch(ranking);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const data = {};
+
+fetch(ranking, {
+  method: 'POST',
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
